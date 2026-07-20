c7a5aaa4c8287bcc315265f13b583c2212ef9404
Hacker01 <hari01012005yadav@gmail.com>
2026-07-20 12:28:09 +0530

    Time: 56 ms (20.64%), Space: 9.5 MB (51.52%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-int ans = 0;
-void func(int num){
-    vector<int> arr;
-    while(num>0){
-        arr.push_back(num%10);
-        num/=10;
-    }
+
+int func(string arr){
+   int cnt = 0;
+    
     int n =arr.size();
-    if(n<3) return;
+
     for(int i=1;i<n-1;i++){
-        if(arr[i]>arr[i+1]&& arr[i]>arr[i-1]) ans++;
-        if(arr[i]<arr[i+1]&& arr[i]<arr[i-1]) ans++;
+        if(arr[i]>arr[i+1]&& arr[i]>arr[i-1]) cnt++;
+        if(arr[i]<arr[i+1]&& arr[i]<arr[i-1]) cnt++;
     }
+    return cnt;
 }
     int totalWaviness(int n1, int n2) {
-        for(int i=n1;i<=n2;i++) func(i);
+        int ans = 0;
+        for(int i=n1;i<=n2;i++){
+            string num = to_string(i);
+            if(num.size()<3) continue;
+            ans+=func(num);
+        }
         return ans;
         
     }
