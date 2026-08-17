27d27cfa323664e776f24d190c502259792aa3ed
Hacker01 <hari01012005yadav@gmail.com>
2026-08-17 10:15:38 +0530

    Time: 0 ms (100%), Space: 10.3 MB (96.9%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -15,27 +15,27 @@ public:
     int minDistance(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0] = i;
-        }
+       vector<int> prev(m+1,0),curr(m+1,0);
+       
         for(int i=0;i<=m;i++){
-            dp[0][i] = i;
+            prev[i] = i;
         }
         for(int i=1;i<=n;i++){
+            curr[0] = i;
             for(int j=1;j<=m;j++){
 
-                if(s[i-1]==t[j-1]) dp[i][j] =  dp[i-1][j-1];
+                if(s[i-1]==t[j-1]) curr[j] =  prev[j-1];
                 else {
-                int ins = dp[i][j-1];
-                int del = dp[i-1][j];
-                int rep = dp[i-1][j-1];
+                int ins = curr[j-1];
+                int del = prev[j];
+                int rep = prev[j-1];
 
-                    dp[i][j] = 1 + min(ins,min(del,rep));
+                    curr[j] = 1 + min(ins,min(del,rep));
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
