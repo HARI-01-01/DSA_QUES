3fc62df13f4f643765a63cdd7648c1592b401cf7
Hacker01 <hari01012005yadav@gmail.com>
2026-09-02 11:32:07 +0530

    Time: 0 ms (100%), Space: 181.1 MB (95.83%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     vector<int> occurrencesOfElement(vector<int>& arr, vector<int>& q, int x) {
         int n = arr.size();
-        unordered_map<int,int> mp;
+        vector<int> pos;
         // idx - x
         int idx = 1;
         for(int i=0;i<n;i++){
             if(arr[i]==x){
-                mp[idx] = i;
-                idx++;
+                pos.push_back(i);
             }
         }
         int m = q.size();
         vector<int> ans(m,-1);
         for(int i=0;i<m;i++){
-            if(mp.find(q[i])!=mp.end()){
-                 ans[i] = mp[q[i]];
+            if(q[i]-1<pos.size()){
+                ans[i] = pos[q[i]-1];
             }
 
         }
