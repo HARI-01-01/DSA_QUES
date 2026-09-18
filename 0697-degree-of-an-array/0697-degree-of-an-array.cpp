c0134976a4962c9881e45cc55bdb0df6620c4335
Hacker01 <hari01012005yadav@gmail.com>
2026-09-18 18:49:30 +0530

    Time: 11 ms (62.9%), Space: 30.1 MB (34.03%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-  int findShortestSubArray(vector<int>& arr) {
-    int n =arr.size();
-    vector<int> vis(50000,0);
-    int cnt = 0;
+    int findShortestSubArray(vector<int>& arr) {
+        unordered_map<int,int> freq;
+        unordered_map<int,int> first;
+        unordered_map<int,int> last;
 
-    for(int i=0;i<n;i++){
-        vis[arr[i]]++;
-        cnt = max(cnt,vis[arr[i]]);
-    }
-    vector<int> freq;
-    for (int i=0;i<50000;i++) {
-        if (vis[i]==cnt) {
-            freq.push_back(i);
+        int n = arr.size();
+        int d = 0;
+        for(int i=0;i<n;i++){
+            int x = arr[i];
+            freq[x]++;
+            if(first.find(x)==first.end()){
+                // not found
+                first[x] = i;
+            }
+            last[x] = i;
+            d = max(d,freq[x]);
         }
-    }
-
-    int ans = n+1;
-    int m = freq.size();
-for (int j=0;j<m;j++) {
-    int str = -1;
-    int end = -1;
-    for(int i=0;i<n;i++){
-        if(freq[j]==arr[i]){
-            if(str==-1){
-                str = i;
-            }else{
-                end = i;
+        int ans = n+1;
+        for(auto i:freq){
+           
+            if(i.second == d){
+                ans = min(ans,last[i.first]-first[i.first]+1);
             }
         }
+        return ans;
+        
     }
-    if (end == -1) ans = min(ans,1);
-    else {
-        ans = min(ans,end-str+1);
-    }
-}
-  return ans;
-
-}
 };
