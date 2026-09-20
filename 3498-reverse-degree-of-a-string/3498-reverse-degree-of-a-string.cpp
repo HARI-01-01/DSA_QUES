4ad0f6897612435bfd0c51a87d6ef8bbb87ad76b
Hacker01 <hari01012005yadav@gmail.com>
2026-09-20 10:39:41 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,9 +4,8 @@ public:
         int ans = 0;
         int n = s.size();
         for(int i=0;i<n;i++){
-            char c = s[i];
             // cout<<26-(c-'a')<<endl;
-            ans+= (i+1)*(26-(c-'a'));
+            ans+= (i+1)*(26-(s[i]-'a'));
         }
         return ans;
     }
